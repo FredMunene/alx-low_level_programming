2aad9237fbe1c6b0482c5e2332fb8bccff9893ba
FredMunene <munene2050gitonga@gmail.com>
2022-06-08 18:43:00 +0000

    changes

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,27 +7,27 @@
  */
 int main(void)
 {
-	int i, j; #declaring two variables
+	int i, j;
 
-	for (i = 0; i < 100; i++) // i starts at zero, is less than 100 and increments by 1.
+	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j < 100; j++) //j starts at zero,is less than 100 and increments by 1.
+		for (j = 0; j < 100; j++)
 		{
-			if (i < j) //i is less than j
+			if (i < j)
 			{
-				putchar((i / 10) + 48); // (i divide by 10)add to 48
-				putchar((i % 10) + 48); // (i modulus 10) add to 48
-				putchar(' '); // puts space between two numbers
-				putchar((j / 10) + 48); // (j divide by 10) add to 48
-				putchar((j % 10) + 48); // (j divide by 10) add to 48
-				if (i != 98 || j != 99)//i is not equal to 98 or j is not equal to 99.
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
-					putchar(',');// comma between pair of integers.
+					putchar(',');
 					putchar(' ');
 				}
 			}
 		}
 	}
-	putchar('\n');// new line
+	putchar('\n');
 	return (0);
 }
