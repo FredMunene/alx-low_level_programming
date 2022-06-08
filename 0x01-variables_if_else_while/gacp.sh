f517fc477019031609611e6e050833d6ce2afbae
FredMunene <munene2050gitonga@gmail.com>
2022-06-08 17:19:06 +0000

    added git pull command

--- 0x01-variables_if_else_while/gacp.sh
+++ 0x01-variables_if_else_while/gacp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+#next feature should add "git pull" but only for the first commit, or do it and not display..
+git pull
+#check what  is the output?
 chmod a+x *
 git add *
 git commit
